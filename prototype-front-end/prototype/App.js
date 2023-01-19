9589a6ffa83782e28fb30dbafe5e27c1e9b99983
Allan DeBoe <adeboe@asu.edu>
2023-01-19 00:01:44 -0700

    (Task 151) created a new React Native project
    I created a new, default React Native project to help begin the work needed to create the front-end.

---
+++ prototype-front-end/prototype/App.js
@@ -0,0 +1,28 @@
+/**
+ * "App.js"
+ *
+ * @author Allan DeBoe
+ * @date January 18th, 2023
+ */
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
