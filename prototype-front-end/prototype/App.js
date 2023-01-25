6f91e7cbb709f0f19a7b5a62aaf57faf3e9d4f44
Allan DeBoe <adeboe@asu.edu>
2023-01-24 20:46:29 -0700

    (Task 151) Created CSS Stylesheet for project
    I created a CSS stylesheet that the React Native prototype will use to stylize the pages. I also fixed I mistake in the README.md where the same content is posted twice.

--- prototype-front-end/prototype/App.js
+++ prototype-front-end/prototype/App.js
@@ -11,15 +11,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// This CSS Module is the Stylesheet used for
+// the prototype. It's there to make it easier
+// to integrate CSS stuff into the React native
+// app
+import styles from './App.module.css';
+
+
 export default function App() {
+	
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.js to start working on your app!</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
+	
 }
 
+/*
 const styles = StyleSheet.create({
 
 	// CONTAINER STYLE
@@ -31,3 +41,4 @@ const styles = StyleSheet.create({
 	},
   
 });
+*/
